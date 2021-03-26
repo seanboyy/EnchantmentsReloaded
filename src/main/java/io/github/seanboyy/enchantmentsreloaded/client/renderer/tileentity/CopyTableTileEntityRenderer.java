452739fb96a3b02ae76feffdf3170ff6e7213454
Seanboyy <sbamford12@gmail.com>
2021-03-25 23:57:05 -0400

    Removed unused import

--- src/main/java/io/github/seanboyy/enchantmentsreloaded/client/renderer/tileentity/CopyTableTileEntityRenderer.java
+++ src/main/java/io/github/seanboyy/enchantmentsreloaded/client/renderer/tileentity/CopyTableTileEntityRenderer.java
@@ -3,7 +3,6 @@ package io.github.seanboyy.enchantmentsreloaded.client.renderer.tileentity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import io.github.seanboyy.enchantmentsreloaded.objects.tileentity.CopyTableTileEntity;
-import io.github.seanboyy.enchantmentsreloaded.objects.tileentity.EnchantmentCraftingTableTileEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.model.BookModel;
